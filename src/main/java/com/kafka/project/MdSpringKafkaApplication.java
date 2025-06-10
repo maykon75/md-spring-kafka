22113065128e25b0890018f7eab1690cf3b5c541
maykon75 <maykonmda2002@gmail.com>
2025-06-10 08:57:27 -0300

    init

---
+++ src/main/java/com/kafka/project/MdSpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdSpringKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdSpringKafkaApplication.class, args);
+	}
+
+}
